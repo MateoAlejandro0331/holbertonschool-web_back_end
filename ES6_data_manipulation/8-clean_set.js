8951271e2b471117553f9aa218e907b227f89b46
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-03-29 17:31:45 -0500

    adding task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,7 @@
 export default function cleanSet(set, startString) {
-  let endString = '';
-  let count = 0;
-  set.forEach((str) => {
-    if (str.includes(startString)) {
-      if (startString === '') return endString;
-      str = str.replace(startString, '');
-      count += 1;
-      if (count === parseInt(set.size - 1)) {
-        endString += str;
-      } else {
-        endString = `${endString}${str}-`;
-      }
-    }
-  });
-  return endString;
+  if (startString === undefined || startString.length === 0) return '';
+  return Array.from(set)
+    .filter((value) => value !== undefined && value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
 }
