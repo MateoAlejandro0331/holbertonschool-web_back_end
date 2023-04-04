045b1185bfc9669d499bf6a46c37973638dfb3d1
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-04 18:24:52 -0500

    adding task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_wait=10):
+async def wait_random(max_wait: int=10) -> float:
     """Wait for a random number of seconds"""
     wait = random.uniform(0, max_wait)
     await asyncio.sleep(wait)
