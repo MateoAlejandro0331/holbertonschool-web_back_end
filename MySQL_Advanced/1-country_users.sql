08dd3fa8d2c3e6359aca4c9e96032a4ee1e45db6
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-13 11:50:13 -0500

    adding task 1

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create user table
+-- because Batch 3 is the best!
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
