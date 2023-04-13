93e6ba031a55733260642e5acfd715ecf82ba097
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-13 11:11:50 -0500

    adding task 0

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- Create user table
 -- because Batch 3 is the best!
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
