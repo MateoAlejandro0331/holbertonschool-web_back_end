ad4300bc24adf2d118e8079aabc8a5040e04a778
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-14 18:13:03 -0500

    adding task 7

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,8 +1,8 @@
 -- script that creates a stored procedure ComputeAverageScoreForUser that computes and store the average
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser (user_id int)
+CREATE PROCEDURE ComputeAverageScoreForUser(user_id int)
 BEGIN 
-  SET @avarege = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
+  SET @avarege = (SELECT AVG(score) FROM corrections GROUP BY corrections.user_id HAVING corrections.user_id = user_id);
   UPDATE users SET average_score = @avarege WHERE id = user_id;
 END $$
 DELIMITER;
