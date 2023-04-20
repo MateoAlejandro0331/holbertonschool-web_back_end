b85f65292ac8defa20a78ad9a6dd02c5113296de
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-20 15:16:42 -0500

    adding task 0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 a start index and an end index corresponding to the range of indexes
 to return in a list for those particular pagination parameters."""
 
+
 def index_range(page, page_size):
     """Returns a tuple"""
     start_index = (page - 1) * page_size
