4ba1b98d63336c580ac4713feeb11b95b670b61d
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-24 18:40:24 -0500

    adding task 3

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -9,6 +9,7 @@ class LRUCache(BasicCache):
         super().__init__()
 
     def put(self, key, item):
+        """Put an item"""
         if key in self.cache_data:
             del self.cache_data[key]
         elif len(self.cache_data) >= self.MAX_ITEMS:
@@ -17,6 +18,7 @@ class LRUCache(BasicCache):
         self.cache_data[key] = item
 
     def get(self, key):
+        """ Get an item by key """
         if key not in self.cache_data:
             return None
         value = self.cache_data.pop(key)
