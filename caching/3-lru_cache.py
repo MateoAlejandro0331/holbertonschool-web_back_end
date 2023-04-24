8d6a2d32013cdfd35788ee820bb02ae10b696a48
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-24 18:35:54 -0500

    adding task 3

---
+++ caching/3-lru_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Define put and get method to access data"""
+BasicCache = __import__('0-basic_cache').BasicCache
+
+
+class LRUCache(BasicCache):
+    """Define LRUCache"""
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        if key in self.cache_data:
+            del self.cache_data[key]
+        elif len(self.cache_data) >= self.MAX_ITEMS:
+            print("DISCARD: {}".format(list(self.cache_data.keys())[0]))
+            del self.cache_data[list(self.cache_data.keys())[0]]
+        self.cache_data[key] = item
+
+    def get(self, key):
+        if key not in self.cache_data:
+            return None
+        value = self.cache_data.pop(key)
+        self.cache_data[key] = value
+        return value
