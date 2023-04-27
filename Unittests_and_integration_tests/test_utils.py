e444eaa7078497db7d03cc940eb0eb6dfbbc8682
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-04-27 17:37:13 -0500

    adding task 0

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,15 +2,18 @@
 """Creating unittests"""
 import unittest
 from parameterized import parameterized
+from utils import access_nested_map
+from typing import Any
 
 
 class TestAccessNestedMap(unittest.TestCase):
     """Testing access nested maps"""
 
-    @parameterized.expand([  # Creatinf cases to pass to the method
-        (2, 3, 5),
-        (1, 1, 2),
-        (0, 0, 0),
-        (-1, 1, 0),
+    @parameterized.expand([  # Creating cases to pass to the method
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, ):
+    def test_access_nested_map(self, value, path, expected) -> Any:
+        """test the correct answer"""
+        self.assertEqual(access_nested_map(value, path), expected)
