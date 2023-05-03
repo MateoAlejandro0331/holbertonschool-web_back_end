70e90410ac500c41900ad581dcd8192e3ed89382
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-03 11:14:45 -0500

    adding task 1

---
+++ i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Creating a flask App"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+@app.route('/')
+def home():
+    """Home page"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    """run the app"""
+    app.run(port=5001)
