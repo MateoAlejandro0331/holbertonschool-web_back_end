7f852951c05df46deca72feb5a6c5fa9640d65d8
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-03 15:11:58 -0500

    adding task 2

---
+++ i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""Creating a flask App"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for Flask app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def home():
+    """Home page"""
+    return render_template('1-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """select a language translation to use for that request:"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    """run the app"""
+    app.run(port=5001)
