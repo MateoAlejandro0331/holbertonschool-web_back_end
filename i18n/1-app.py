71414b2af9fd4b7ddcf66a7b6b76df0270289e1e
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-03 11:16:20 -0500

    fixed task 1

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -8,12 +8,14 @@ babel = Babel(app)
 
 
 class Config(object):
+    """Config class for Flask app"""
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
+
 
 @app.route('/')
 def home():
