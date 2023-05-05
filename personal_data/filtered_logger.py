a925911571ac5f158ee1059e65444bc2fb0f681e
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-05 15:21:28 -0500

    adding task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,6 +8,7 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Function to filter data"""
     for field in fields:
-        newMessage = re.sub("(?<={:s}=)(.*?)(?={:s})".format(field, separator),
+        newMessage = re.sub("(?<={}=)(.*?)(?={})".format(field, separator),
                             redaction, message)
+        message = newMessage
     return newMessage
