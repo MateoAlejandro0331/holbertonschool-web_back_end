2ec0ba5cdc4d442b3f0dd343dc97d6045992cc32
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-05 15:16:21 -0500

    adding task 0

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """filtered logger"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """Function to filter data"""
     for field in fields:
         newMessage = re.sub("(?<={:s}=)(.*?)(?={:s})".format(field, separator),
