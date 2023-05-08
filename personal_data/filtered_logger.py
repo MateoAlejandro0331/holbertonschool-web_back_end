3921634c78309130d67062b0292c37b27765cf75
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-07 23:34:33 -0500

    adding files

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,11 @@
 """filtered logger"""
 import re
 from typing import List
+import logging
+import os
+import mysql.connector
+
+PII_FIELDS = ('name', 'phone', 'ssn', 'password', 'email')
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -12,3 +17,57 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                             redaction, message)
         message = newMessage
     return newMessage
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Init Redacting Formatter"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Formats the class object"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    '''Returns a logger object'''
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    ch = logging.StreamHandler()
+    logger.setFormatter(RedactingFormatter(PII_FIELDS))
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ returns a connector to the database PERSONAL_DATA_DB_NAME """
+    user = os.getenv('PERSONAL_DATA_DB_USERNAME')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD')
+    host = os.getenv('PERSONAL_DATA_DB_HOST')
+    database = os.getenv('PERSONAL_DATA_DB_NAME')
+    return mysql.connector.connect(user=user, password=password,
+                                   host=host, database=database)
+
+
+def main():
+    """ main function """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    fields = [i[0] for i in cursor.description]
+    logger = get_logger()
+    for row in cursor:
+        message = ""
+        for i in range(len(fields)):
+            message += f"{fields[i]}={row[i]};"
+        logger.info(message)
+    cursor.close()
+    db.close()
