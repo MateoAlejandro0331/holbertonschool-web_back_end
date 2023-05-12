8a65ac2790ac1d58d6e12587fdc40b94c6893d75
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-12 17:10:24 -0500

    adding task 5

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -21,6 +21,7 @@ if auth:
 
 @app.before_request
 def authentication():
+    """function that run bofere any move"""
     if auth is None:
         return
     paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
