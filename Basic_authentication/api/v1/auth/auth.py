06eae4e2641180171812cc1f11fe1167e838eaeb
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-12 11:50:16 -0500

    adding task 4

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,6 +9,16 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication"""
+        if path is None:
+            return True
+        if len(excluded_paths) == 1 and "/api/v1/status/" in excluded_paths \
+                and path == "/api/v1/status":
+            return False
+        if path not in excluded_paths:
+            return True
+        if not excluded_paths or excluded_paths is None:
+            return True
+ 
         return False
 
     def authorization_header(self, request=None) -> str:
