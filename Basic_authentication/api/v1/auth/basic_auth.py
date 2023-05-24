7a1429accef1cd474ffb6b50bc8ed92cc8e5c020
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-24 11:04:29 -0500

    adding task 7

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,4 +5,15 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic authentication"""
-    pass
+
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> str:
+        """Methos to check authorization"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        strings = authorization_header.split()
+        if strings[0] != 'Basic':
+            return None
+        return strings[1]
