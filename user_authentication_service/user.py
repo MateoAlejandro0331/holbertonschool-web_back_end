5dc7cbe1dc4c297ad4f4e1ab024d76bea810c290
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-30 18:50:41 -0500

    changes id field

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -11,7 +11,7 @@ class User(Base):
     """Creating the class and associated with the database"""
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
