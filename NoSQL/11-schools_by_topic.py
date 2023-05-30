67205c45dc657a6cd57012aa79519b6a7899e1ff
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-30 15:29:18 -0500

    adding task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -4,4 +4,4 @@
 
 def schools_by_topic(mongo_collection, topic):
     """Find a school with a given topic"""
-    return mongo_collection.find({'topic': {'$in': [topic]}})
+    return mongo_collection.find({'topics': {'$in': [topic]}})
