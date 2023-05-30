2e8497785c867755a27446f236997062cb56266a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-30 08:25:28 -0500

    adding task 6

---
+++ ES6_classes/6-sky_high.js
@@ -0,0 +1,25 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  get sqft() { return this._sqft; }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = value;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors.`;
+  }
+}
