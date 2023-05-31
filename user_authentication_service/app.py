30678840691e0ecac85e11e6c51ad3fca32f7927
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-31 10:20:04 -0500

    adding task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, abort, request
 from auth import Auth
 
 
@@ -16,7 +16,7 @@ def welcome():
 
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
-def user():
+def users():
     """Check if a user exists or creates a new one"""
     email = request.form.get('email')
     password = request.form.get('password')
@@ -25,6 +25,23 @@ def user():
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def sessions():
+    """create a new session for the specified user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            response = jsonify(
+                {"email": email, "message": "logged in"})
+            response.set_cookie('session_id', session_id)
+            return response
+        abort(401)
+    except BaseException:
+        abort(401)
 
 
 if __name__ == "__main__":
