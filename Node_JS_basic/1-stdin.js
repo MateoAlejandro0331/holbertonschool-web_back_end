79917dd53c5c90b4924ce5168401a07cbb7e1d24
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-31 16:01:07 -0500

    adding task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -4,7 +4,8 @@ console.log('Welcome to Holberton School, what is your name?');
 
 process.stdin.on('data', (data) => {
   const input = data.trim(); // Remove trailing newline and convert to lowercase
-  console.log(`Your name is: ${input}`);
+  if (input)
+    process.stdout.write(`Your name is: ${input}\n`);
 });
 
 process.on('exit', () => {
