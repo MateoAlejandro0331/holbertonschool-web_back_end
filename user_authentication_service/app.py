b3437432dcb5338fcf7ae8752657b5ad09d3ec36
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-31 14:41:16 -0500

    adding task 19

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -87,7 +87,7 @@ def update_password():
     new_password = request.form.get('new_password')
 
     try:
-        AUTH.update_password(reset_token, email, new_password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({'email': email, 'message': 'Password updated'}), 200
     except ValueError:
         abort(403)
