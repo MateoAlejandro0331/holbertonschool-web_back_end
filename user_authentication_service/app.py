f730ad577d6943332652ebf7a9bf2e28d3f88e18
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-31 10:58:20 -0500

    adding task 14

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -49,9 +49,9 @@ def logout():
     """Logout the user and destroy the session"""
     session_id = request.cookies.get('session_id')
     if session_id:
-        user_id = AUTH.get_user_from_session_id(session_id)
-        if user_id:
-            AUTH.destroy_session(user_id)
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
             return redirect('/')
     return jsonify({'message': 'Forbidden'}), 403
 
