e547cef1c70596c82ad2e0679cea10c00f18e530
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-05-31 09:13:36 -0500

    adding task 7

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -17,14 +17,14 @@ def welcome():
 
 @app.route('/users', methods=['POST'], strict_slashes=False)
 def user():
+    """Check if a user exists or creates a new one"""
     email = request.form.get('email')
     password = request.form.get('password')
     try:
         AUTH.register_user(email, password)
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
-        return jsonify({"message": "email already registered"}), 400 
-
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
