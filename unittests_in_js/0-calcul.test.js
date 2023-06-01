b660f3435a1367e2924046780fad55dea2d92edc
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-06-01 15:45:49 -0500

    adding task 0

---
+++ unittests_in_js/0-calcul.test.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul')
+
+// Test suite
+describe('testing calculateNumber', () => {
+  // Test cases
+  it('result should be 4', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+  });
+
+  it('result should be 5', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+  });
+
+  it('result should be 5', () => {
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+  });
+
+  it('result should be 5', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
