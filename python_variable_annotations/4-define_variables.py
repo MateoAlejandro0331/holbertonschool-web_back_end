9855c7702ec5c07405448ad85294d13168192599
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-06-02 12:00:25 -0500

    adding task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This is a demonstration of variable declaration and annotation.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
