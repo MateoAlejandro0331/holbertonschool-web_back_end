137cbf362793f721f4146d0daf779437e7263313
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-06-02 11:36:55 -0500

    adding task 3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -15,6 +15,17 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        inputs_key = f"{method.__qualname__}:inputs"
+        outputs_key = f"{method.__qualname__}:outputs"
+        self._redis.rpush(inputs_key, str(args))
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, str(output))
+        return output
+    return wrapper
+
 class Cache:
     """cache class"""
     def __init__(self):
@@ -22,7 +33,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """store data in cache"""
         key = str(uuid.uuid4())
