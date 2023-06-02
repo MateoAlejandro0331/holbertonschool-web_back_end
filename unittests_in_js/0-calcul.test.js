41f33fe19c7cac56e4aa24dc0351bbb982a3bda6
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-06-02 10:45:44 -0500

    adding task 0

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -5,18 +5,21 @@ const calculateNumber = require('./0-calcul')
 describe('testing calculateNumber', () => {
   // Test cases
   it('result should be 4', () => {
-    assert.equal(calculateNumber(1, 3), 4);
+    assert.equal(calculateNumber(3, 1), 4);
   });
-
   it('result should be 5', () => {
     assert.equal(calculateNumber(1, 3.7), 5);
   });
-
+  it('result should be 5', () => {
+    assert.equal(calculateNumber(1.2, 3.7), 5);
+  });
   it('result should be 6', () => {
-    assert.equal(calculateNumber(1.6, 3.7), 6);
+    assert.equal(calculateNumber(1.5, 3.7), 6);
   });
-
-  it('result should be 5', () => {
-    assert.equal(calculateNumber(1, 3.7), 5);
+  it('result should be 0', () => {
+    assert.equal(calculateNumber(-1, 1), 0);
+  });
+  it('result should be 0', () => {
+    assert.equal(calculateNumber(0, 0), 0);
   });
 });
