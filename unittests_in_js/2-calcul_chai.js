18a17d0c314c867e8fc9932c02585e6474a4090e
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-06-05 10:52:56 -0500

    adding task 2

--- unittests_in_js/2-calcul_chai.js
+++ unittests_in_js/2-calcul_chai.js
@@ -1,15 +1,20 @@
 function calculateNumber(type, a, b) {
-  switch (type) {
-    case 'SUM':
-      return Math.round(a) + Math.round(b);
-    case 'SUBTRACT':
-      return Math.round(a) - Math.round(b);
-    case 'DIVIDE':
-      if (Math.round(b) === 0) {
-        return 'Error';
-      }
-      return Math.round(a) / Math.round(b);
+  const roundedA = Math.round(a);
+  const roundedB = Math.round(b);
+  console.log(roundedA + " " + roundedB);
+  if (type === 'SUM') {
+    return roundedA + roundedB;
+  } else if (type === 'SUBTRACT') {
+    return roundedA - roundedB;
+  } else if (type === 'DIVIDE') {
+    if (roundedB === 0) {
+      return 'Error';
+    }
+    return roundedA / roundedB;
+  } else {
+    throw new Error('Invalid type');
   }
 }
+
 
 module.exports = calculateNumber;
