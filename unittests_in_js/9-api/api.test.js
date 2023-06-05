67cf52e8f44fed741f13106e1741a12ad2b368a7
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2023-06-05 17:47:05 -0500

    adding all

---
+++ unittests_in_js/9-api/api.test.js
@@ -0,0 +1,37 @@
+const chai = require('chai');
+const expect = chai.expect;
+const request = require('request');
+
+describe('API', () => {
+  describe('GET /', () => {
+    it('should return 200', (done) => {
+      request('http://localhost:7865', (err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should return the correct result', (done) => {
+      request('http://localhost:7865', (err, res, body) => {
+        expect(body).to.equal('Welcome to the payment system');
+        done();
+      });
+    });
+  });
+
+  describe('GET /cart/:id', () => {
+    it('should return 200 when :id is a number', (done) => {
+      request('http://localhost:7865/cart/123', (err, res) => {
+        expect(res.statusCode).to.equal(200);
+        done();
+      });
+    });
+
+    it('should return 404 when :id is NOT a number', (done) => {
+      request('http://localhost:7865/cart/abc', (err, res) => {
+        expect(res.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
+});
